81377a22d06a11914e52393fa6f4bde9e9909f76
hherrera <hherrera@araujoysegovia.com>
2013-11-13 02:00:41 +0000

    inicial de sugarcrm

---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2013-11-08 18:08:35
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
